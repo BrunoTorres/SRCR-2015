4655affdcd14197ce96c0bb296fc0d488a1c1034
Bruno Pereira <bruno.torres.pereira@gmail.com>
2015-04-22 16:43:16 +0100

    no message

--- TP2/SRCR/src/srcr/SRCR.java
+++ TP2/SRCR/src/srcr/SRCR.java
@@ -14,14 +14,11 @@ import se.sics.jasper.Query;
 import se.sics.jasper.SICStus;
 import se.sics.jasper.SPException;
 
-
 public class SRCR {
 
- 
-    protected static SICStus sp; 
+    protected static SICStus sp;
 
-   
-     public SRCR(String pathToFile){
+    public SRCR(String pathToFile) {
         try {
             loadSICStus();
             System.out.println(pathToFile);
@@ -31,60 +28,68 @@ public class SRCR {
             System.exit(1);
         }
     }
-      private static void loadSICStus() throws SPException { 
-        sp = new SICStus(); 
-    }
+
     
-    public Boolean check(String queryS){
+
+    public Boolean check(String queryS) {
         try {
-            HashMap map = new HashMap(); 
-            return sp.query(queryS,map);
+            HashMap map = new HashMap();
+            return sp.query(queryS, map);
         } catch (SPException ex) {
             //Logger.getLogger(Prolog.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
     }
-    
-    public List<String> getStringResults(String queryS) throws InterruptedException, Exception{
-       
+
+    public List<String> getStringResults(String queryS) throws InterruptedException, Exception {
+
         ArrayList<String> results = new ArrayList<>();
-            //String queryS = “predicate(‘term’,X).”;
-            HashMap map = new HashMap();
-            Query query = null;
+        //String queryS = “predicate(‘term’,X).”;
+        HashMap map = new HashMap();
+        Query query = null;
         try {
-            query = sp.openPrologQuery(queryS,map);
+            query = sp.openPrologQuery(queryS, map);
         } catch (SPException ex) {
             Logger.getLogger(SRCR.class.getName()).log(Level.SEVERE, null, ex);
         }
         try {
             while (query.nextSolution()) {
                 //System.out.println(map.toString());
-                
+
                 results.add(map.toString());
-            } 
+            }
         } catch (Exception ex) {
             Logger.getLogger(SRCR.class.getName()).log(Level.SEVERE, null, ex);
         }
-            query.close();
-       
+        query.close();
+
         return results;
     }
-    
-    public static void loadSICStusScrpt(String path) throws SPException{
+    private static void loadSICStus() throws SPException {
+        sp = new SICStus();
+    }
+
+    public static void loadSICStusScrpt(String path) throws SPException {
         sp.load(path);
-        
+
     }
-    
-     public static void main(String[] args) throws SPException {
-         //loadSICStus("/Users/brunopereira/Documents/SourceTree/SRCR/Tp2/TP2.pl");
-            loadSICStus();
+
+    public static void main(String[] args) throws SPException, InterruptedException, Exception {
+        loadSICStus();
+        loadSICStusScrpt("/Users/brunopereira/Documents/SourceTree/SRCR/Tp2/TP2.pl");
+
+        HashMap map = new HashMap();
+        Query query = sp.openPrologQuery("listening.", map);
+        while (query.nextSolution()) {
+            System.out.println(map.toString());
+        }
+            query.close();
+
+            
             System.out.println("/Users/brunopereira/Documents/SourceTree/SRCR/Tp2/TP2.pl");
-            loadSICStusScrpt("/Users/brunopereira/Documents/SourceTree/SRCR/Tp2/TP2.pl\"");
-         System.out.println("oks");
-         
-         
+            //loadSICStusScrpt("/Users/brunopereira/Documents/SourceTree/SRCR/Tp2/TP2.pl\"");
+            System.out.println("oks");
+
+        }
+
     }
-    
-    
-    
-}
